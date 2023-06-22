50fa88234f23b0892f49a07547ecab3d7cafaae9
Evandro Venancio <evandro.venancio@gmail.com>
2023-06-22 18:15:23 -0300

    Adjustments

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/main.css');
   eleventyConfig.addPassthroughCopy('src/shared.css');
+  eleventyConfig.addPassthroughCopy('src/admin');
 
   return {
     dir: {
